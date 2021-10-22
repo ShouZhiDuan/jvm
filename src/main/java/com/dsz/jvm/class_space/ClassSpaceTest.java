8261e7e6b50ebb501d8b9dcaa60f6e6b8f4e95a2
段守志 <dszazhy1314@163.com>
2021-10-22 11:39:47 +0800

    计算实例大小，单位bytes

---
+++ src/main/java/com/dsz/jvm/class_space/ClassSpaceTest.java
@@ -0,0 +1,9 @@
+package com.dsz.jvm.class_space;
+
+/**
+ * @Auther: ShouZhi@Duan
+ * @Date: 2021/10/22 11:27
+ * @Description:
+ */
+public class ClassSpaceTest {
+}
---
+++ src/main/java/com/dsz/jvm/class_space/TestMain.java
@@ -0,0 +1,9 @@
+package com.dsz.jvm.class_space;
+
+/**
+ * @Auther: ShouZhi@Duan
+ * @Date: 2021/10/22 11:30
+ * @Description:
+ */
+public class TestMain {
+}
