6b3a88d92a9a1a61a72a9c63eb49738903f0718b
段守志 <dszazhy1314@163.com>
2021-10-22 11:40:08 +0800

    fix code

--- src/main/java/com/dsz/jvm/class_space/ClassSpaceTest.java
+++ src/main/java/com/dsz/jvm/class_space/ClassSpaceTest.java
@@ -2,8 +2,52 @@ package com.dsz.jvm.class_space;
 
 /**
  * @Auther: ShouZhi@Duan
- * @Date: 2021/10/22 11:27
  * @Description:
  */
 public class ClassSpaceTest {
+    private String aString;
+    private Integer bInteger;
+    private int cInt;
+    public double ddouble;
+    public Double eDouble;
+
+    public String getaString() {
+        return aString;
+    }
+
+    public void setaString(String aString) {
+        this.aString = aString;
+    }
+
+    public Integer getbInteger() {
+        return bInteger;
+    }
+
+    public void setbInteger(Integer bInteger) {
+        this.bInteger = bInteger;
+    }
+
+    public int getcInt() {
+        return cInt;
+    }
+
+    public void setcInt(int cInt) {
+        this.cInt = cInt;
+    }
+
+    public double getDdouble() {
+        return ddouble;
+    }
+
+    public void setDdouble(double ddouble) {
+        this.ddouble = ddouble;
+    }
+
+    public Double geteDouble() {
+        return eDouble;
+    }
+
+    public void seteDouble(Double eDouble) {
+        this.eDouble = eDouble;
+    }
 }
--- src/main/java/com/dsz/jvm/class_space/TestMain.java
+++ src/main/java/com/dsz/jvm/class_space/TestMain.java
@@ -1,9 +1,15 @@
 package com.dsz.jvm.class_space;
 
+import org.openjdk.jol.info.ClassLayout;
+
 /**
  * @Auther: ShouZhi@Duan
- * @Date: 2021/10/22 11:30
  * @Description:
  */
 public class TestMain {
+    public static void main(String[] args) {
+        ClassSpaceTest spaceTest = new ClassSpaceTest();
+        spaceTest.setaString("abc");
+        System.out.println(ClassLayout.parseInstance(spaceTest).toPrintable());
+    }
 }
