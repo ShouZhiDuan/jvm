f36080192308afe4b7952e983d3e17d3fcc5b3d1
段守志 <dszazhy1314@163.com>
2021-10-22 12:00:15 +0800

    fix code

--- src/main/java/com/dsz/jvm/class_space/TestMain.java
+++ src/main/java/com/dsz/jvm/class_space/TestMain.java
@@ -10,6 +10,8 @@ public class TestMain {
     public static void main(String[] args) {
         ClassSpaceTest spaceTest = new ClassSpaceTest();
         spaceTest.setaString("abc");
+        spaceTest.setcInt(6666);
         System.out.println(ClassLayout.parseInstance(spaceTest).toPrintable());
+        System.out.println("哈希码：" + spaceTest.hashCode());
     }
 }
