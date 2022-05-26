4a272807f86241fa772f8a3a7247d664692b88ab
段守志 <dszazhy1314@163.com>
2022-05-26 16:07:41 +0800

    fix code

--- src/main/java/com/dsz/jvm/JvmApplication.java
+++ src/main/java/com/dsz/jvm/JvmApplication.java
@@ -1,29 +1,32 @@
 package com.dsz.jvm;
 
-import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryPoolMXBean;
 
 @SpringBootApplication
 public class JvmApplication {
 
-    public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(JvmApplication.class);
-        springApplication.setBannerMode(Banner.Mode.OFF);
-        springApplication.run(args);
-        while (true){
-            //获取当前堆的大小 byte 单位
-            long heapSize = Runtime.getRuntime().totalMemory();
-            System.out.println("当前堆大小：" + heapSize);
-            //获取堆的最大大小byte单位
-            //超过将抛出 OutOfMemoryException
-            long heapMaxSize = Runtime.getRuntime().maxMemory();
-            System.out.println("最大堆大小：" + heapMaxSize);
-            //获取当前空闲的内存容量byte单位
-            long heapFreeSize = Runtime.getRuntime().freeMemory();
-            System.out.println("当前空闲堆大小：" + heapFreeSize);
+//    public static void main(String[] args) {
+//        SpringApplication springApplication = new SpringApplication(JvmApplication.class);
+//        springApplication.setBannerMode(Banner.Mode.OFF);
+//        springApplication.run(args);
+//    }
 
+    /**
+     * 输出内存数据使用情况
+     */
+    public static void main(String[] args) {
+        byte[] bytes = new byte[1024*1024*3];
+        for (MemoryPoolMXBean memoryPoolMXBean : ManagementFactory.getMemoryPoolMXBeans()){
+            System.out.println(memoryPoolMXBean.getName()
+            + "  已使用：" + memoryPoolMXBean.getUsage().getUsed()/1024/1024 + "MB"
+            + "  总大小：" + memoryPoolMXBean.getUsage().getCommitted()/1024/1024 + "MB");
         }
     }
+
+
+
 
 }
