23ad4816a2ca7ca7503e8b251b18378f780d4aa0
段守志 <dszazhy1314@163.com>
2022-05-27 16:17:09 +0800

    添加基本使用参数列表

--- src/main/java/com/dsz/jvm/JvmApplication.java
+++ src/main/java/com/dsz/jvm/JvmApplication.java
@@ -1,30 +1,35 @@
 package com.dsz.jvm;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryPoolMXBean;
 
 @SpringBootApplication
 public class JvmApplication {
 
-//    public static void main(String[] args) {
-//        SpringApplication springApplication = new SpringApplication(JvmApplication.class);
-//        springApplication.setBannerMode(Banner.Mode.OFF);
-//        springApplication.run(args);
-//    }
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(JvmApplication.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run(args);
+        while (true) {
+            System.out.println("===");
+            byte[] bytes = new byte[1024 * 1024];
+        }
+
+
+    }
 
     /**
      * 输出内存数据使用情况
      */
-    public static void main(String[] args) {
-        byte[] bytes = new byte[1024*1024*3];
-        for (MemoryPoolMXBean memoryPoolMXBean : ManagementFactory.getMemoryPoolMXBeans()){
-            System.out.println(memoryPoolMXBean.getName()
-            + "  已使用：" + memoryPoolMXBean.getUsage().getUsed()/1024/1024 + "MB"
-            + "  总大小：" + memoryPoolMXBean.getUsage().getCommitted()/1024/1024 + "MB");
-        }
-    }
+//    public static void main(String[] args) {
+//        byte[] bytes = new byte[1024*1024*3];
+//        for (MemoryPoolMXBean memoryPoolMXBean : ManagementFactory.getMemoryPoolMXBeans()){
+//            System.out.println(memoryPoolMXBean.getName()
+//            + "  已使用：" + memoryPoolMXBean.getUsage().getUsed()/1024/1024 + "MB"
+//            + "  总大小：" + memoryPoolMXBean.getUsage().getCommitted()/1024/1024 + "MB");
+//        }
+//    }
 
 
 
